351655b4cdfabfcca096e98bd75e6525e05cd70b
qiaolinfei <qiaolinfei@zhihu.com>
2018-09-01 00:36:42 +0800

    add resetful method

---
+++ src/main/java/com/afeiluo/hello/exception/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.afeiluo.hello.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    public EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
---
+++ src/main/java/com/afeiluo/hello/model/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.afeiluo.hello.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
