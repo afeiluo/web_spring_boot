70b0e9c15c1ae312101345256b18b76d5223dc7f
qiaolinfei <qiaolinfei@zhihu.com>
2018-09-03 09:56:41 +0800

    add getter/settter to entity

--- src/main/java/com/afeiluo/hello/model/Employee.java
+++ src/main/java/com/afeiluo/hello/model/Employee.java
@@ -10,9 +10,37 @@ import javax.persistence.Id;
 @Entity
 public class Employee {
 
-    private @Id @GeneratedValue Long id;
+
+    private @Id
+    @GeneratedValue
+    Long id;
     private String name;
     private String role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     Employee(String name, String role) {
         this.name = name;
